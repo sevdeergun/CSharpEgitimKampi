3d40995711c19a15c3a4c485b6b8c52499d22e98
sevdeergun <ergunnsevde@gmail.com>
2025-03-08 13:42:51 +0300

    07_ForeachLoop

---
+++ 07_ForeachLoop/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07_ForeachLoop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region foreach döngüsü
+            //string[] cities = { "milano", "roma", "istanbul" };
+            //foreach (string x in cities)
+            //{
+            //    Console.WriteLine(x);
+            //}
+            //-----------------------------------------------------
+            //int[] numbers = { 45, 65, 62, 32, 8, 2, 8754, 88, 51, 58, 98, 49 };
+            //foreach (int number in numbers)
+            //{
+            //    if (number % 2 == 0)
+            //    {
+            //        Console.WriteLine(number);
+            //    }
+            //}
+            //----------------------------------------------------
+            //int[] numbers = { 45, 65, 62, 32, 8, 2, 8754, 88, 51, 58, 98, 49 };
+            //int total = 0;
+            //foreach (int i in numbers) {total += i;}
+            //Console.WriteLine(total);
+            //------------------------------------------------------
+
+            //List<int> numbers = new List<int>()
+            //{
+            //    1,2,3,4,5,6,8
+            //};
+            //foreach (int number in numbers) {Console.WriteLine(number); }
+            //#endregion
+            //Console.Read();
+             #endregion
+        }
+    }
+}
